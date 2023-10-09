c43bf9a9e2746787af187cd59b258f0c090e7419
YannickMyxe <shawn.froste.sf@gmail.com>
2023-10-09 23:52:47 +0200

    increased resolution

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Bevy game engine".into(),
-                resolution: (800., 600.).into(),
+                resolution: (1280., 720.).into(),
                 ..default()
             }),
             ..default()
